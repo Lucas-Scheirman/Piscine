7bee49e727e53a7f63c794b64a44abd7fef32ebc
Lucas Scheirman <lscheirm@fu-r2-p1.s19.be>
2026-02-19 04:42:16 +0100

    sds

--- C04/ex04/ft_putnbr_base.c
+++ C04/ex04/ft_putnbr_base.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lscheirm <lscheirm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/18 18:16:35 by lscheirm          #+#    #+#             */
-/*   Updated: 2026/02/18 23:52:07 by lscheirm         ###   ########.fr       */
+/*   Created: 2026/02/19 00:56:58 by lscheirm          #+#    #+#             */
+/*   Updated: 2026/02/19 02:09:46 by lscheirm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,27 +36,12 @@ void	write_result(int index, long nbr_long, char *base)
 	tab_resultat[index_2] = '\0';
 	write_index_base(tab_resultat, index_2 - 1);
 }
-
-void	ft_putnbr_base(int nbr, char *base)
+void base_true (char *base, long nbr_long)
 {
 	int	index;
 	int	index_2;
-	long nbr_long;
 	index = 0;
 	index_2 = 0;
-	nbr_long = nbr;
-	
-	if(!(char_true(base,nbr_long)))
-	{
-		return;
-	}
-	if(nbr_long<0)
-	{
-		write(1, "-", 1);
-		nbr_long = -nbr_long;
-	}
-		
-	
 	while (base[index])
 	{
 		if ((base[index] == '+') || (base[index] == '-') || (base[index] == ' ' && (base[index] <= 126)))
@@ -71,26 +56,27 @@ void	ft_putnbr_base(int nbr, char *base)
 		index_2 = 0;
 		index++;
 	}
-	
 	write_result(index, nbr_long, base);
 }
-int char_true (char *base, long nbr_long)
+void	ft_putnbr_base(int nbr, char *base)
 {
-	if ((base[0]== '\0') || (base[1] == '\0'))
-		return 0;
+	long nbr_long;
+	nbr_long = nbr;
 	
+	if ((base[0]== '\0') || (base[1] == '\0'))
+		return;
 	if(nbr_long == 0)
 	{
 		write(1, "0", 1);
-		return 0;
+		return;
 	}
-	if (base[1] == '\0')
-		return 0;
-	return 1;
+	if(nbr_long<0)
+	{
+		write(1, "-", 1);
+		nbr_long = -nbr_long;
+	}
+	base_true(base,nbr_long);
 }
-
-
-
 int	main(void)
 {
 	ft_putnbr_base(-2147483647, "fas");
--- C04/ex05/ft_atoi_base.c
+++ C04/ex05/ft_atoi_base.c
@@ -1,43 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lscheirm <lscheirm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/19 00:56:58 by lscheirm          #+#    #+#             */
+/*   Updated: 2026/02/19 04:39:33 by lscheirm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+
+
+
+
 int ft_atoi_base(char *str, char *base)
 {
-    
-	int	index;
-	int	index_2;
-	long nbr_long;
-    int i=0
-	index = 0;
-	index_2 = 0;
-	
-	if (base[index] == '\0')
-		return ;
-	
-	if(nbr_long == 0)
+	int index;
+	int index_2;
+	int nb;
+	int len_base;
+	nb=0;
+	index=0;
+	index_2=0;
+	while (str[index])
 	{
-		write(1, "0", 1);
-		return ;
+		index++;	
 	}
-	if (base[1] == '\0')
-		return ;
-	
-	if(nbr_long<0)
+	len_base=index;
+	index=0;
+	while(str[index])
 	{
-		write(1, "-", 1);
-		nbr_long = -nbr_long;
-	}
-    while (base[index])
-	{
-		if ((base[index] == '+') || (base[index] == '-') || (base[index] == ' ' && (base[index] <= 126)))
-    		return;
-
-		while (base[index_2])
+		while(base[index_2])
 		{
-			if (base[index] == base[index_2] && index != index_2)
-				return ;
+			if(str[index]==base[index_2])
+			{
+				nb = (nb * len_base) + (index_2);
+			}
 			index_2++;
 		}
-		index_2 = 0;
+		index_2=0;
 		index++;
 	}
-    
-    
+	return nb;
 }
+int main()
+{
+	printf("%d",ft_atoi_base("ghjgh","ghj"));
+}
+
+
+/*
+Écrivez une fonction qui convertit la partie initiale de la chaîne pointée par `str`
+
+en une représentation entière.
+
+• `str` est dans une base spécifique, donnée en deuxième paramètre.
+
+• À l'exception de la base, la fonction doit se comporter exactement comme `ft_atoi`.
+
+• Si un argument invalide est fourni, la fonction doit renvoyer 0.
+
+Exemples d'arguments invalides :
+
+◦ La base est vide ou ne contient qu'un seul caractère.
+
+◦ La base contient des caractères dupliqués.
+
+◦ La base contient les caractères « + », « - » ou des espaces.
+
+• Le prototype de la fonction doit être le suivant :
+*/
